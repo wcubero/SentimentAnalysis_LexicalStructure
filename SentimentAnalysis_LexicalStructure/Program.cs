abd82ceae16f4f5f2ee02f5ebc75b542c8284681
wcubero <wcubero@hotmail.com>
2022-11-02 11:26:24 -0600

    initial commit

---
+++ SentimentAnalysis_LexicalStructure/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SentimentAnalysis_LexicalStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messagetotest = @"
+hola si estoy interesado
+";
+
+            string[] positives = File.ReadAllLines("positive_words_es.txt");
+            string[] negatives = File.ReadAllLines("negative_words_es.txt");
+             
+            double positive = 0;
+            double negative = 0;
+
+            foreach(string s in messagetotest.Trim().Split(' '))
+            {
+                if(positives.Any(e=>e.Equals(s.ToLower())))
+                {
+                    positive = positive + 1;
+                }else if (negatives.Any(e => e.Equals(s.ToLower())))
+                {
+                    negative = negative + 1;
+                }
+            }
+            double score = (positive - negative) / (positive + negative);
+            Console.WriteLine(score);
+            Console.Read();
+        }
+    }
+}
